ebae264d82b3f2786c7bc7a6a96a8c594e520703
Hossam Mahmoud <mr.hossam50@gmail.com>
2024-10-02 10:05:06 +0300

    fixes

--- Certification.Domain/DomainModels/PagedResultViewModel.cs
+++ Certification.Domain/DomainModels/PagedResultViewModel.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace Certification.Domain.DomainModels
 {
     public class PagedResultViewModel<T>
     {
-        public List<T> Items { get; set; } = new List<T>();
+        public IPagedList<T> Items { get; set; } = new List<T>().ToPagedList();
         public int TotalCount { get; set; }
         public int CurrentPage { get; set; }
         public int ItemsPerPage { get; set; }
